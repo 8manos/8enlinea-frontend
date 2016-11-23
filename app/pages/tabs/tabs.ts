206bb48326c4845c3c1bf33c16eb27dbc87ac53f
Anabelle Handdoek <ana@gathacol.net>
2016-11-23 11:49:35 -0500

    mas tabs

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -12,12 +12,13 @@ export class TabsPage {
   public tab1Root: any;
   public tab2Root: any;
   public tab3Root: any;
+  public tab4Root: any;
 
   constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
     this.tab1Root = HomePage;
-    this.tab2Root = ChatPage;
+    this.tab4Root = ChatPage;
     this.tab3Root = ContactPage;
   }
 }
